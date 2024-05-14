89491f85894dfd33af6108a96744697f671d2714
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-05-14 14:44:30 +0200

    feat: add Movie.php

--- index.php
+++ index.php
@@ -1,34 +1,5 @@
 <?php
-class Movie {
-    public $title;
-    public $genre;
-    public $duration;
-    public $releaseDate;
-
-    public static $genres = [
-        'fantasy', 'action', 'family', 'comedy'
-    ];
-
-    // Costruttore che richiede i paramentri obbligatori
-    public function __construct(string $_title, $_genre, string $_duration, string $_releaseDate){
-        $this->title = $_title;
-        $this->genre = $_genre;
-        $this->duration = $_duration . ' ' . 'min';
-        $this->releaseDate = $_releaseDate;
-    }
-
-    // Funzione che torna una stringa con tutte le info del film
-    public function getAllInfoFilm(){
-        $title = $this->title;
-        $genre = $this->genre;
-        $duration = $this->duration;
-        $releaseDate = $this->releaseDate;
-
-        return $title . ' it\'s a ' . $genre . ' film ' . 'released in ' . $releaseDate . ' with a running time of ' . $duration . '.';
-    }
-
-    
-}
+require_once __DIR__ . './Models/Movie.php';
 
 // Messaggio con le info del primo film
 $firstMovie = new Movie('Ritorno al futuro', implode(', ', Movie::$genres), 116, '03-07-1985');
